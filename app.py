313a081bf4350b3c80cdd491d42af2ce0c06deef
Haim Skira <haimsk@bynet.co.il>
2023-02-16 02:27:49 +0200

    put dont work from the web

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-CORS(app)
+#CORS(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}}, allow_methods=["*"])
 
 class Student(db.Model):
@@ -16,19 +16,13 @@ class Student(db.Model):
     def __repr__(self):
         return f"Student({self.id}, {self.name}, {self.age})"
 
-@app.route('/student', methods=['POST'])
-def add_student():
-    student_data = request.get_json()
-    student = Student(name=student_data['name'], age=student_data['age'])
-    db.session.add(student)
-    db.session.commit()
-    return {'id': student.id, 'name': student.name, 'age': student.age}
-
+#GET
 @app.route('/student/<int:student_id>', methods=['GET'])
 def get_student(student_id):
     student = Student.query.get(student_id)
     return {'id': student.id, 'name': student.name, 'age': student.age}
 
+#GET All Student
 @app.route('/student', methods=['GET'])
 def display_all():
     res=[]
@@ -36,6 +30,16 @@ def display_all():
         res.append({'id': stu.id, 'name': stu.name, 'age': stu.age})
     return jsonify (res)
 
+#POST
+@app.route('/student', methods=['POST'])
+def add_student():
+    student_data = request.get_json()
+    student = Student(name=student_data['name'], age= student_data['age'])
+    db.session.add(student)
+    db.session.commit()
+    return {'id': student.id, 'name': student.name, 'age': student.age}
+
+#Update
 @app.route('/student/<int:student_id>', methods=['PUT'])
 def update_student(student_id):
     student_data = request.get_json()
@@ -45,12 +49,20 @@ def update_student(student_id):
     db.session.commit()
     return {'id': student.id, 'name': student.name, 'age': student.age}
 
+#DELETE
 @app.route('/student/<int:student_id>', methods=['DELETE'])
 def delete_student(student_id):
     student = Student.query.get(student_id)
     db.session.delete(student)
     db.session.commit()
     return {'id': student.id, 'name': student.name, 'age': student.age}
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 if __name__ == '__main__':
     with app.app_context():
