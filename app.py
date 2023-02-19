87b7ce4218e05a61dcfe77cf66734cc1094c8ae3
Haim Skira <haimsk@bynet.co.il>
2023-02-19 17:30:29 +0200

    host

--- app.py
+++ app.py
@@ -57,12 +57,12 @@ def delete_student(student_id):
     db.session.commit()
     return {'id': student.id, 'name': student.name, 'age': student.age}
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    return response
+# @app.after_request
+# def after_request(response):
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+#     return response
 
 if __name__ == '__main__':
     with app.app_context():
